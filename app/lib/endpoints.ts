bfc4413bdb9748601b6062e31d09a2abaaa4607e
aidogg <ab@versori.io>
2022-01-25 01:17:51 +0000

    chore: add endpoints to a global file

---
+++ app/lib/endpoints.ts
@@ -0,0 +1,5 @@
+export const getAllPeopleEndpoint = () =>
+  "https://run.mocky.io/v3/ceb09528-8228-4a95-b7d9-c1f945023c92"
+
+export const getPersonEndpoint = (id: string) =>
+  `https://run.mocky.io/v3/45928af0-9bd1-4eb0-a9a1-55845a009e8d/${id}`
